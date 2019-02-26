835cf559c78501d44f8c5af3b9ba645c136a7b3f
kouchao <admin@jskou.com>
2019-02-26 18:20:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      text: 'hellow vue'
+    }
+  }
+};
+</script>
+
+<style>
+  #app {
+    color: #f00
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // 它会应用到普通的 `.js` 文件
+      // 以及 `.vue` 文件中的 `<script>` 块
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // 它会应用到普通的 `.css` 文件
+      // 以及 `.vue` 文件中的 `<style>` 块
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({template: './public/index.html'})
+  ]
+}
